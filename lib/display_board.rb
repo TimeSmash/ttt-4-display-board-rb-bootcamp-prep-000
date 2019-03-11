4c08dc1ccb508a8fcfe09544a1346b2449122a37
mcumm64 <mcumm64@gmail.com>
2019-03-11 22:13:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -14,8 +14,7 @@ def display_board (board_array)
   #start with top-left corner, and index-0 of board
   #ex. board[0] = O, want O to be in top-left
   
-  tic_tac =
-  puts "   |   |   "
+  puts "  #{board[0} ] |  #{board[1]} | #{board[2]}  "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
