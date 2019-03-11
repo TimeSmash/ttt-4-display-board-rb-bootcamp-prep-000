9c933df26b261b66cc1e391a8e1df122cd08e242
mcumm64 <mcumm64@gmail.com>
2019-03-11 22:11:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board =
+tic_tac =
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -13,4 +13,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board (board_array)
   #start with top-left corner, and index-0 of board
   #ex. board[0] = O, want O to be in top-left
+  
+  tic_tac =
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
   
